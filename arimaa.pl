db45c266556800c1c4ef0226ad4d10eb1b2c4f34
dolfinsbizou <jorandon@gmail.com>
2017-06-06 16:28:12 +0200

    fonctions utilitaires get_pieces et get_opponents

--- arimaa.pl
+++ arimaa.pl
@@ -15,4 +15,11 @@
 % get_moves(Moves, [silver, []], [[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[1,5,horse,silver],[1,6,dog,silver],[1,7,cat,silver],[2,7,rabbit,gold],[6,0,cat,gold],[6,1,horse,gold],[6,2,camel,gold],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]).
 
 % default call
-get_moves([[[1,0],[2,0]],[[0,0],[1,0]],[[0,1],[0,0]],[[0,0],[0,1]]], Gamestate, Board).
+get_opponents([],[]).
+get_opponents([[Y,X,N,gold]|Queue],[[Y,X,N,gold]|Remaining]):- get_opponents(Queue, Remaining).
+get_opponents(Queue,[[Y,X,N,silver]|Remaining]):- get_opponents(Queue, Remaining).
+get_pieces([],[]).
+get_pieces([[Y,X,N,gold]|Queue],[[Y,X,N,silver]|Remaining]):- get_opponents(Queue, Remaining).
+get_pieces(Queue,[[Y,X,N,gold]|Remaining]):- get_opponents(Queue, Remaining).
+
+get_moves([[[1,0],[2,0]],[[2,0],[2,1]],[[2,1],[3,1]],[[3,1],[3,2]]], Gamestate, Board).
