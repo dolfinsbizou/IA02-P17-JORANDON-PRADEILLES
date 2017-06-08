c87662c1111959e8455e739e36119297270fe08a
dolfinsbizou <jorandon@gmail.com>
2017-06-09 00:09:18 +0200

    Added functions

--- arimaa.pl
+++ arimaa.pl
@@ -2,8 +2,6 @@
       [  get_moves/3
       ]).
 	
-% A few comments but all is explained in README of github
-
 % get_moves signature
 % get_moves(Moves, gamestate, board).
 
@@ -14,12 +12,51 @@
 % Call exemple:
 % get_moves(Moves, [silver, []], [[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[1,5,horse,silver],[1,6,dog,silver],[1,7,cat,silver],[2,7,rabbit,gold],[6,0,cat,gold],[6,1,horse,gold],[6,2,camel,gold],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]).
 
-% default call
+%%% Fonctions utilitaires %%%
+
+% concat_list(Result, List1, List2) : concatène deux listes. %
+concat_list(Y, [], Y).
+concat_list([X1|L], [X1|X2], Y):- concat_list(L, X2, Y).
+
+% length_list(Result, List) : retourne la taille d'une liste. %
+% TODO
+
+% gen_numeric(Result, Begin, End) : Génère un entier entre les deux bornes. %
+gen_numeric(Result, Result, _).
+gen_numeric(Result, Min, Max):- Min2 is Min+1, Min2 =< Max, gen_numeric(Result, Min2, Max).
+
+% get_opponents(Result, Board) : récupère les opposants sur le board. %
 get_opponents([],[]).
 get_opponents([[Y,X,N,gold]|Queue],[[Y,X,N,gold]|Remaining]):- get_opponents(Queue, Remaining).
-get_opponents(Queue,[[Y,X,N,silver]|Remaining]):- get_opponents(Queue, Remaining).
-get_pieces([],[]).
-get_pieces([[Y,X,N,gold]|Queue],[[Y,X,N,silver]|Remaining]):- get_opponents(Queue, Remaining).
-get_pieces(Queue,[[Y,X,N,gold]|Remaining]):- get_opponents(Queue, Remaining).
+get_opponents(Queue,[[_,_,_,silver]|Remaining]):- get_opponents(Queue, Remaining).
 
-get_moves([[[1,0],[2,0]],[[2,0],[2,1]],[[2,1],[3,1]],[[3,1],[3,2]]], Gamestate, Board).
+% get_pieces : récupère ses propres pièces (silver) dans le paramètre 2 et les retourne dans le paramètre 1. %
+get_pieces([],[]).
+get_pieces([[Y,X,N,silver]|Queue],[[Y,X,N,silver]|Remaining]):- get_pieces(Queue, Remaining).
+get_pieces(Queue,[[_,_,_,gold]|Remaining]):- get_pieces(Queue, Remaining).
+
+% get_by_type : récupère une pièce par type (paramètre 3) dans le paramètre 2 et les retourne dans le paramètre 1. %
+get_by_type([],[],_).
+get_by_type([[Y,X,Type,S]|Queue],[[Y,X,Type,S]|Remaining], Type):- get_by_type(Queue, Remaining, Type).
+get_by_type(Queue,[[_,_,_,_]|Remaining], Type):- get_by_type(Queue, Remaining, Type).
+
+% get_adjacent_pieces : Retourne dans le paramètre 1 la liste des pièces adjacentes à la pièce paramètre 2 du board dans le paramètre 3. %
+get_adjacent_pieces([],_,[]).
+get_adjacent_pieces(Result, Piece, [Curr|Remaining]). % TODO
+
+% distance : Compute la distance entre deux couples de coordonnées. %
+distance(D, [X1, Y1], [X2, Y2]):- gen_numeric(X2, 0, 7), gen_numeric(Y2, 0, 7), abs(X2-X1, DX), abs(Y2-Y1, DY), D is DX+DY. %TODO ajouter gen numeric pour X1 Y1
+
+% get_moves_for_given_piece : Retourne dans le paramètre 1 la liste des coups possibles pour une pièce du paramètre 2 sur le board dans le paramètre 3. %
+%get_possible_moves([], _, []).
+get_possible_moves(Result, Piece, Board):- setof([X,Y], get_possible_move([X,Y], Piece, Board), Result).
+get_possible_move([X2,Y2], [X1,Y1,_,_], Board):- distance(D, [X1, Y1], [X2, Y2]), D < 5. %TODO
+
+% get_dangerous_holes_by_side : une fonction qui retourne les positions des trous noirs sans pièces d'un type donné autour. La question étant de pouvoir savoir si un trou noir présente un risque pour le type donné. TODO %
+
+% get_nearest_piece : une fonction qui pour une position donnée retourne la pièce la plus proche. TODO %
+
+%%% Moteur de jeu %%%
+
+get_moves([[[Y,X],[Y2,X]],[[Y2,X],[Y3,X]],[[Y3,X],[Y4,X]],[[Y4,X],[Y5,X]]], Gamestate, Board):- get_pieces(Pieces, Board), get_by_type([[Y,X,_,_]|_], Pieces, rabbit), Y2 is Y+1, Y3 is Y2+1, Y4 is Y3+1, Y5 is Y4+1, get_opponents([], Board).
+get_moves([[[1,0],[2,0]],[[2,0],[1,0]]], Gamestate, Board).
